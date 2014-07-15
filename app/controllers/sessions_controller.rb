fffb810cd3318c20247636a1226c4bdda99786c9
ChrisMLee <cristovao.m.lee@gmail.com>
2014-07-14 20:42:31 -0400

    got rid of access headers in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,6 @@ class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [ :create ]
   respond_to :json
 
-   after_action :access_control_headers
-
-   def set_access_control_headers
-     headers['Access-Control-Allow-Origin'] = "*"
-     headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
-   end
-
   def create
     user = User.authenticate(params[:email], params[:password])
     if user
