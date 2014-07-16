b17e84f4f0bfb4cfd1a41931a968cbc3e8f5c0fb
ChrisMLee <cristovao.m.lee@gmail.com>
2014-07-16 19:35:49 -0400

    formatted responses to jsonp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   respond_to :json
 
-  # skip_before_filter :verify_authenticity_token, only: [ :create ]
+  skip_before_filter :verify_authenticity_token
 
   def index
     #needs to come with maps
@@ -10,9 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render :json=> {:success=>true, :email=>@user.email, :id=>@user.id}
+      render :json=> {:success=>true, :email=>@user.email, :id=>@user.id}, :callback => params['callback'], :status => 200
     else
-      render :json=> {:success=>false}
+      render :json=> {:success=>false}, :callback => params['callback'], :status => 200
     end
   end
 
