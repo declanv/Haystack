26b880e269f4bebe20d8345cdf9640d43dc5a9ec
ChrisMLee <cristovao.m.lee@gmail.com>
2014-07-17 07:29:03 -0400

    seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,12 +27,58 @@ map = Map.create({
   map_long: -73.9896807
 })
 
-pin_one = Pin.create({
+Pin.create({
   map_id: map.id,
   name: "vinnies pizza",
   description: "good pizza",
   photo_url: "http://vinniesnewyorkpizza.com/images/web/walloffame.jpg",
-  pin_lat: 40.77118185975647,
-  pin_long: -74.1796875
+  pin_lat: 40.6848036615912,
+  pin_long: -73.9215087890625
 })
+
+Pin.create({
+  map_id: map.id,
+  name: "my favorite bench",
+  description: "its nice",
+  photo_url: "http://vinniesnewyorkpizza.com/images/web/walloffame.jpg",
+  pin_lat: 40.728527124206,
+  pin_long: -73.9393615722656
+})
+
+Pin.create({
+  map_id: map.id,
+  name: "hot dog on a stick",
+  description: "its nice",
+  photo_url: "http://vinniesnewyorkpizza.com/images/web/walloffame.jpg",
+  pin_lat: pin_lat: 40.7066689811733,
+  pin_long: -74.1810607910156
+})
+
+Pin.create({
+  map_id: map.id,
+  name: "fred flinstone's house",
+  description: "its nice",
+  photo_url: "http://vinniesnewyorkpizza.com/images/web/walloffame.jpg",
+  pin_lat: pin_lat: 40.6952166135172,
+  pin_long: -74.3980407714844
+})
+
+Pin.create({
+  map_id: map.id,
+  name: "juice bar",
+  description: "its nice",
+  photo_url: "http://vinniesnewyorkpizza.com/images/web/walloffame.jpg",
+  pin_lat: pin_lat: 40.7569774907177,
+  pin_long: -73.9853668212891
+})
+
+Pin.create({
+  map_id: map.id,
+  name: "a dog walk",
+  description: "its nice",
+  photo_url: "http://vinniesnewyorkpizza.com/images/web/walloffame.jpg",
+  pin_lat: pin_lat: 40.7424124233837,
+  pin_long: -73.9826202392578
+})
+
 
