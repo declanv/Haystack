a16d17ee0aba5565482b0d5373854bfdc84557a9
Crustacean87! <declanv@gmail.com>
2014-07-30 18:27:31 -0400

    Checking

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,10 +4,11 @@ class PinsController < ApplicationController
   def create
     pin = Pin.create(pin_params)
     respond_with pin
+    # Comment to see if I can commmit
   end
 
   def pin_params
-    params.require(:pin).permit(:description, :photo_url, :pin_lat, :pin_long)
+    params.require(:pin).permit(:name, :description, :photo_url, :pin_lat, :pin_long, :map_id)
   end
 end
 
