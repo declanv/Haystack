d05ac225af54377cd4119773e625b8b2595328bc
Crustacean87! <declanv@gmail.com>
2014-07-30 20:07:32 -0400

    Added in render json for pins create route in pin controller.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,9 +4,11 @@ class PinsController < ApplicationController
   respond_to :json
 
   def create
+    puts pin_params
     pin = Pin.create(pin_params)
-    respond_with pin
+    # respond_with pin
     # Comment to see if I can commmit
+    render :json => pin.to_json, :callback => params['callback']
   end
 
   def pin_params
